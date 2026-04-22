e452dcd84401ab2ba99ac95f68681b43c04a7c2b
Aadesh Chaudhari <aadeshchaudhari14@gmail.com>
2026-04-23 04:27:40 +0530

    The threat Intel Blueprint file

---
+++ core/include/threat_intel.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+
+class ThreatIntel {
+public:
+
+    static std::string resolve_ipv4(const std::string& ip_str);
+    
+    static bool is_malicious(const std::string& hostname);
+};
