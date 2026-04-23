da9982b9134dd0a859045e4f3ac6ac56fd1fe8f6
Aadesh Chaudhari <aadeshchaudhari14@gmail.com>
2026-04-24 02:30:12 +0530

    Add IPv4 resolution function to ThreatIntel class

--- core/src/threat_intel.cpp
+++ core/src/threat_intel.cpp
@@ -3,6 +3,18 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
+std::string ThreatIntel::resolve_ipv4(const std::string& ip_str) {
+    struct sockaddr_in sa;
+    sa.sin_family = AF_INET;
+    inet_pton(AF_INET, ip_str.c_str(), &sa.sin_addr);
+
+    char host[NI_MAXHOST];
+    if (getnameinfo((struct sockaddr*)&sa, sizeof(sa), host, sizeof(host), NULL, 0, NI_NAMEREQD) == 0) {
+        return std::string(host);
+    }
+    return "UNKNOWN IP";
+}
+
 bool ThreatIntel::is_malicious(const std::string& hostname) {
     if (hostname.find("evil.com") != std::string::npos) return true;
     if (hostname.find("pastebin.com") != std::string::npos) return true;
