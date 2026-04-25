bd48f3d652aa98057a8a6a0393e3eed5df5ee87e
Aadesh Chaudhari <aadeshchaudhari14@gmail.com>
2026-04-26 01:22:48 +0530

    Implement execution event handling in eBPF program to track process creation and parent process details

--- core/src/bpf/shadow.bpf.c
+++ core/src/bpf/shadow.bpf.c
@@ -5,16 +5,21 @@
 
 #define EVENT_TYPE_NET 1
 #define EVENT_TYPE_FILE 2
+#define EVENT_TYPE_EXEC 3
 
 struct event_t {
-    u8 type; // 1 = Network, 2 = File
+    u8 type;
     u32 pid;
+    u32 ppid;
     
+    // Network Specific
     u32 dest_ip;
     u16 dest_port;
     u16 family; 
     
+    // File/Exec Specific
     char filename[256];
+    char comm[16];
 };
 
 struct {
@@ -37,9 +42,7 @@ int trace_connect(struct trace_event_raw_sys_enter *ctx) {
         bpf_probe_read_user(&addr, sizeof(addr), uservaddr);
         
         u8 first_byte = addr.sin_addr.s_addr & 0xFF;
-        if (first_byte == 127 || first_byte == 10) {
-            return 0;
-        }
+        if (first_byte == 127 || first_byte == 10) return 0;
 
         struct event_t *e = bpf_ringbuf_reserve(&rb, sizeof(*e), 0);
         if (!e) return 0;
@@ -54,15 +57,11 @@ int trace_connect(struct trace_event_raw_sys_enter *ctx) {
     } else if (family == 10) {
         struct event_t *e = bpf_ringbuf_reserve(&rb, sizeof(*e), 0);
         if (!e) return 0;
-
         e->type = EVENT_TYPE_NET;
         e->pid = pid;
         e->family = family;
-        e->dest_ip = 0;
-        e->dest_port = 0;
         bpf_ringbuf_submit(e, 0);
     }
-
     return 0;
 }
 
@@ -78,10 +77,35 @@ int trace_openat(struct trace_event_raw_sys_enter *ctx) {
     e->type = EVENT_TYPE_FILE;
     e->pid = pid;
 
-    // ctx->args[1] holds the pointer to the filename string in user memory
     const char *pathname = (const char *)ctx->args[1];
-    
     bpf_probe_read_user_str(&e->filename, sizeof(e->filename), pathname);
+
+    bpf_ringbuf_submit(e, 0);
+    return 0;
+}
+
+//HOOK 3: The Execution Kill Chain
+SEC("tracepoint/syscalls/sys_enter_execve")
+int trace_execve(struct trace_event_raw_sys_enter *ctx) {
+    u64 id = bpf_get_current_pid_tgid();
+    u32 pid = id >> 32;
+
+    struct task_struct *task = (struct task_struct *)bpf_get_current_task();
+    u32 ppid = BPF_CORE_READ(task, real_parent, tgid);
+
+    struct event_t *e = bpf_ringbuf_reserve(&rb, sizeof(*e), 0);
+    if (!e) return 0;
+
+    e->type = EVENT_TYPE_EXEC;
+    e->pid = pid;
+    e->ppid = ppid;
+
+    // Grab the name of the parent process (e.g., "npm" or "sh")
+    bpf_get_current_comm(&e->comm, sizeof(e->comm));
+
+    // Grab the path of the new binary being launched (e.g., "/usr/bin/cat")
+    const char *arg_ptr = (const char *)ctx->args[0];
+    bpf_probe_read_user_str(&e->filename, sizeof(e->filename), arg_ptr);
 
     bpf_ringbuf_submit(e, 0);
     return 0;
