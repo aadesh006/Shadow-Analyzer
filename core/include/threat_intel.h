4448a44145b56504fb23e32256c8d189b0268d68
Aadesh Chaudhari <aadeshchaudhari14@gmail.com>
2026-05-16 15:28:54 +0530

    Refactor ThreatIntel class: improve IP resolution and enhance malicious domain detection

--- core/include/threat_intel.h
+++ core/include/threat_intel.h
@@ -1,11 +1,9 @@
 #pragma once
 #include <string>
-#include <vector>
 
 class ThreatIntel {
 public:
-
     static std::string resolve_ipv4(const std::string& ip_str);
-    
     static bool is_malicious(const std::string& hostname);
+    static bool is_trusted_cdn(const std::string& hostname);
 };
--- core/src/threat_intel.cpp
+++ core/src/threat_intel.cpp
@@ -9,20 +9,51 @@ std::string ThreatIntel::resolve_ipv4(const std::string& ip_str) {
     inet_pton(AF_INET, ip_str.c_str(), &sa.sin_addr);
 
     char host[NI_MAXHOST];
-    if (getnameinfo((struct sockaddr*)&sa, sizeof(sa), host, sizeof(host), NULL, 0, NI_NAMEREQD) == 0) {
+    if (getnameinfo((struct sockaddr*)&sa, sizeof(sa),
+                    host, sizeof(host),
+                    NULL, 0, NI_NAMEREQD) == 0) {
         return std::string(host);
     }
-    return "UNKNOWN IP";
+    return ip_str;
+}
+
+bool ThreatIntel::is_trusted_cdn(const std::string& ip_or_host) {
+    static const char* trusted[] = {
+        "registry.npmjs.org",
+        "githubusercontent.com",
+        "github.com",
+        "nodejs.org",
+        "npmjs.com",
+        "cloudflare.com",
+        "fastly.net",
+        "amazonaws.com",
+        "104.16.", "104.17.", "104.18.", "104.19.",
+        "104.20.", "104.21.",
+        "172.66.", "172.67.",
+        nullptr
+    };
+    for (int i = 0; trusted[i] != nullptr; i++) {
+        if (ip_or_host.find(trusted[i]) != std::string::npos)
+            return true;
+    }
+    return false;
 }
 
 bool ThreatIntel::is_malicious(const std::string& hostname) {
-    std::vector<std::string> malicious_domains = {
-        "evil.com", "pastebin.com", "ngrok.io", "localtunnel.me", 
-        "requestbin.net", "burpcollaborator.net", "interact.sh"
+    static const char* malicious[] = {
+        "sfrclak.com",
+        "git-tanstack.com",
+        "getsession.org",
+        "ngrok.io",
+        "localtunnel.me",
+        "requestbin.net",
+        "burpcollaborator.net",
+        "interact.sh",
+        nullptr
     };
-
-    for (const auto& domain : malicious_domains) {
-        if (hostname.find(domain) != std::string::npos) return true;
+    for (int i = 0; malicious[i] != nullptr; i++) {
+        if (hostname.find(malicious[i]) != std::string::npos)
+            return true;
     }
     return false;
 }
