26c2c6c1d80bec78c8fefe29ada7dc52be27b45b
Aadesh Chaudhari <aadeshchaudhari14@gmail.com>
2026-05-17 16:45:47 +0530

    Removed the dual execution of sandbox

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -60,7 +60,6 @@ if (!target.empty() && target[0] == '/') {
         };
         
         std::cout << COLOR_CYAN << "[Shadow]" << COLOR_RESET << " Launching " << pkg_manager << " inside sandbox..." << std::endl;
-        sandbox.run(pkg_manager, args);
         
         // std::cout << "[Shadow] Invoking Behavioral Parser..." << std::endl;
         // Parser parser;
